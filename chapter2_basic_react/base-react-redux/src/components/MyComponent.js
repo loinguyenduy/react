e9321a2b4715bc8164f73955ce2b016a62cc5d06
Nguyen Duy Loi <nguyenduyloi04@gmail.com>
2025-09-22 00:18:41 +0700

    video 17

--- chapter2_basic_react/base-react-redux/src/components/MyComponent.js
+++ chapter2_basic_react/base-react-redux/src/components/MyComponent.js
@@ -4,11 +4,24 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  //state is object
+  state = {
+    name: "Loi",
+    address: "Hanoi",
+    age: 21,
+  };
+
+  info = {
+    name: "HoidanIT",
+    address: "Hanoi",
+    age: 26,
+  };
   //JSX: Viết code JS trong code của HTML
-  render (){
-    return(
-      <div>My first component
-        {Math.random()}
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} and I come from {this.state.address} 
+        My name is {this.info.name} and I come from {this.info.address} 
       </div>
     );
   }
