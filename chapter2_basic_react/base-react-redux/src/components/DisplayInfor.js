dea2a61b895d05d481963cdad7bcf981c9a75a9c
Nguyen Duy Loi <nguyenduyloi04@gmail.com>
2025-09-28 16:01:00 +0700

    'video25_ifelse'

--- chapter2_basic_react/base-react-redux/src/components/DisplayInfor.js
+++ chapter2_basic_react/base-react-redux/src/components/DisplayInfor.js
@@ -1,39 +1,62 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+  state = {
+    isShowListUser: true
+  }
 
-  render(){
+  handleShowHide = () => {
+    this.setState({
+      isShowListUser: !this.state.isShowListUser //toggle: 
+    })
+  };
+  render() {
     //props: truyền từ cha sang con
-    console.log(this.props)
+    console.log(this.props);
     //props => stand for property
-    //use destructuring 
-    const {listUsers} = this.props
-    console.log(listUsers)
-    return(
+    //use destructuring
+    const { listUsers } = this.props;
+    console.log(listUsers);
+    return (
       <div>
-        {/* Dùng map để lặp các phần tử trong object state, sẽ in ra 3 phần tử có trong object */}
-        {listUsers.map((user, index) => {
-          // console.log(user)
-          return (
-            // cần 1 thuộc tính key (qua id) để định danh thẻ div
-            <div key={user.id}>
-              <div>My name is {user.name}  </div>
-              <div>My age is {user.age} </div>
-              <hr/>
-              </div>
-          )
-        })}
+        <div>
+          <span
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowListUser === true ? "Hide list users:" : "Show list users:"}
+          </span>
+        </div>
+        {this.state.isShowListUser && (
+          <div>
+            {/* Dùng map để lặp các phần tử trong object state, sẽ in ra 3 phần tử có trong object */}
+            {listUsers.map((user, index) => {
+              console.log("Check map user:", user);
 
-        {/* <div>My name is {name}</div>
-        <div>My age is {age}</div> <hr/>
-
-        <div>My age is {age}</div> 
-        <div>My age is {age}</div> <hr/>
-
-        <div>My age is {age}</div> 
-        <div>My age is {age}</div>  */}
+              return (
+                <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
+                  <div>My name is {user.name} </div>
+                  <div>My age is {user.age} </div>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
-    )
+    );
+
+    {
+      /* <div>My name is {name}</div>
+        <div>My age is {age}</div> <hr/>
+
+        <div>My age is {age}</div> 
+        <div>My age is {age}</div> <hr/>
+
+        <div>My age is {age}</div> 
+        <div>My age is {age}</div>  */
+    }
   }
 }
 
--- chapter2_basic_react/base-react-redux/src/components/MyComponent.js
+++ chapter2_basic_react/base-react-redux/src/components/MyComponent.js
@@ -8,9 +8,9 @@ import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
   state = {
     listUser: [
-      { id: 1, name: "Hoi dan IT", age: "30" },
+      { id: 1, name: "Hoi dan IT", age: "16" },
       { id: 2, name: "Duy Loi", age: "21" },
-      { id: 3, name: "Harry Pham Dev", age: "26" },
+      { id: 3, name: "Harry Pham Dev", age: "69" },
     ],
   };
 
