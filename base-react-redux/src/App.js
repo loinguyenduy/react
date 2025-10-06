73cc972b5c974edb6d551a73e127840f80e7d550
Nguyen Duy Loi <nguyenduyloi04@gmail.com>
2025-10-07 00:50:52 +0700

    'video47_designnav

---
+++ base-react-redux/src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Header from "./components/Header/Header";
+
+
+const App = () => {
+
+  return (
+    <div className="app-container">
+      <Header/>
+    </div>
+  );
+};
+
+export default App;
