134532295c5940114132526790e0a3c72e8cbef6
Nguyen Duy Loi <nguyenduyloi04@gmail.com>
2025-10-07 16:17:39 +0700

    'video50_activeLink'

--- base-react-redux/src/components/Header/Header.js
+++ base-react-redux/src/components/Header/Header.js
@@ -1,9 +1,8 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => {
   return (
@@ -14,9 +13,17 @@ const Header = () => {
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Link to="/" className='nav-link'>Home</Link>
-            <Link to="/users" className='nav-link'>Users</Link>
-            <Link to="/admins" className='nav-link'>Admin</Link>
+            {/* NavLink is used to create links that are aware of the current route.
+              It will automatically apply an "active" class to the link when the route matches. */}
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/users" className="nav-link">
+              Users
+            </NavLink>
+            <NavLink to="/admins" className="nav-link">
+              Admin
+            </NavLink>
 
             {/* <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/users">Users</Nav.Link>
@@ -26,9 +33,7 @@ const Header = () => {
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Log out
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
@@ -36,6 +41,6 @@ const Header = () => {
       </Container>
     </Navbar>
   );
-}
+};
 
 export default Header;
