b965c15ec732358fe64b569f0f26df33b9efec7e
Nguyen Duy Loi <nguyenduyloi04@gmail.com>
2025-11-05 00:10:55 +0700

    'video80_reduxPersist'

--- project-quizz/base-react-redux/src/components/Auth/Login.js
+++ project-quizz/base-react-redux/src/components/Auth/Login.js
@@ -48,7 +48,7 @@ const Login = () => {
       dispatch(doLogin(data));
       toast.success(data.EM);
       setIsLoading(false);
-      // navigate("/");
+      navigate("/");
     }
 
     if (data && +data.EC !== 0) {
--- project-quizz/base-react-redux/src/utils/axiosCustomize.js
+++ project-quizz/base-react-redux/src/utils/axiosCustomize.js
@@ -19,6 +19,7 @@ instance.interceptors.request.use(
     return config;
   },
   function (error) {
+    NProgress.start();
     // Do something with request error
     return Promise.reject(error);
   }
@@ -33,6 +34,7 @@ instance.interceptors.response.use(
     return response && response.data ? response.data : response;
   },
   function (error) {
+    NProgress.done();
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return error && error.response
