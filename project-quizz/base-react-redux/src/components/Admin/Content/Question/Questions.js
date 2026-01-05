8cdc37ffe88af6dd26c61447c4535b3fa05776ad
Nguyen Duy Loi <nguyenduyloi04@gmail.com>
2026-01-06 01:37:46 +0700

    '99_validate_add_new'

--- project-quizz/base-react-redux/src/components/Admin/Content/Question/Questions.js
+++ project-quizz/base-react-redux/src/components/Admin/Content/Question/Questions.js
@@ -14,9 +14,10 @@ import {
   postCreateNewAnswerForQuestion,
   postCreateNewQuestionForQuiz,
 } from "../../../../services/apiServices";
+import { toast } from "react-toastify";
 
 const Questions = (props) => {
-  const [questions, setQuestions] = useState([
+  const initQuestions = [
     {
       id: uuidv4(),
       description: "",
@@ -30,7 +31,8 @@ const Questions = (props) => {
         },
       ],
     },
-  ]);
+  ]
+  const [questions, setQuestions] = useState(initQuestions);
 
   const [isPreviewImage, setIsShowPreviewImage] = useState(false);
 
@@ -162,31 +164,69 @@ const Questions = (props) => {
 
   const handleSubmitQuestionForQuiz = async () => {
     //validate data
+    if (_.isEmpty(selectedQuiz)) {
+      toast.error("Please choose a Quiz!");
+      return;
+    }
 
-    console.log("question: ", selectedQuiz);
+    //validate answer
+    let isValidAnswer = true;
+    let indexQ = 0,
+      indexA = 0;
+    for (let i = 0; i < questions.length; i++) {
+      for (let j = 0; j < questions[i].answers.length; j++) {
+        if (!questions[i].answers[j].description) {
+          isValidAnswer = false;
+          indexA = j;
+          break;
+        }
+      }
+      indexQ = i;
+      if (isValidAnswer === false) break;
+    }
+
+    if (isValidAnswer === false) {
+      toast.error(`Not empty Answer ${indexA + 1} at Question ${indexQ + 1}`);
+      return;
+    }
+
+    //validate question
+
+    let isValidQ = true;
+    let indexQ1 = 0;
+    for (let i = 0; i < questions.length; i++) {
+      if (!questions[i].description) {
+        isValidQ = false;
+        indexQ1 = i;
+        break;
+      }
+    }
+
+    if (isValidQ === false) {
+      toast.error(`Not empty description for Question ${indexQ1 + 1}`);
+      return;
+    }
+
     //submit question
-    await Promise.all(
-      questions.map(async (question) => {
-        const q = await postCreateNewQuestionForQuiz(
-          +selectedQuiz.value,
-          question.description,
-          question.imageFile
-        );
+    for (const question of questions) {
+      const q = await postCreateNewQuestionForQuiz(
+        +selectedQuiz.value,
+        question.description,
+        question.imageFile
+      );
 
-        //submit answer
-        await Promise.all(
-          question.answers.map(async (answer) => {
-            await postCreateNewAnswerForQuestion(
-              answer.description,
-              answer.isCorrect,
-              q.DT.id
-            );
-          })
+      //submit answers
+      for (const answer of question.answers) {
+        await postCreateNewAnswerForQuestion(
+          answer.description,
+          answer.isCorrect,
+          q.DT.id
         );
-      })
-    );
+      }
+    }
 
-    //submit answers
+    toast.success('Create questions and answers successfully.')
+    setQuestions(initQuestions)
   };
 
   const handlePreviewImage = (questionId) => {
